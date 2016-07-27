99e4c2c937466305312e2c99a14582fca789e264
Chris Rollins <chris.rollins@leapfire.com>
2016-07-27 12:00:06 -0700

    routes update with create event routes

--- app/config/routes.py
+++ app/config/routes.py
@@ -5,3 +5,5 @@ routes['/profile']                = 'Users#index'
 routes['/logout']                 = 'Users#logout'
 routes['/review']                 = 'Reviews#index'
 routes['/event/<event_id>']       = 'Events#index'
+routes['/event/new']              = 'Events#createPage' #use to link to the event creation page when the user wants to create a new event
+routes['POST']['/event/create']   = 'Events#create' #only used by the event creation page, do not use
